4c36ec70846b83c79c412b188f44902bd5f133a6
clliao <yorwxue@gmail.com>
2018-01-04 09:19:23 +0800

    2018/01/04

--- train_tripletloss.py
+++ train_tripletloss.py
@@ -470,7 +470,7 @@ def parse_arguments(argv):
     # Setting of performance
     parser.add_argument('--num_of_threads', type=int,
                         help='Depend on performance of computer.',
-                        default=16)
+                        default=4)
     return parser.parse_args(argv)
 
 
