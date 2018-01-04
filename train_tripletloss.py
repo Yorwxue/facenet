ea8b36ba451da2f6198a659270f544325b829bab
clliao <yorwxue@gmail.com>
2018-01-04 09:29:11 +0800

    2018/01/04

--- train_tripletloss.py
+++ train_tripletloss.py
@@ -416,11 +416,11 @@ def parse_arguments(argv):
     parser.add_argument('--max_nrof_epochs', type=int,
                         help='Number of epochs to run.', default=500)
     parser.add_argument('--batch_size', type=int,
-                        help='Number of images to process in a batch.', default=90)
+                        help='Number of images to process in a batch.', default=80)
     parser.add_argument('--image_size', type=int,
                         help='Image size (height, width) in pixels.', default=160)
     parser.add_argument('--people_per_batch', type=int,
-                        help='Number of people per batch.', default=45)
+                        help='Number of people per batch.', default=40)
     parser.add_argument('--images_per_person', type=int,
                         help='Number of images per person.', default=40)
     parser.add_argument('--epoch_size', type=int,
